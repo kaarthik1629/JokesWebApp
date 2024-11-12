4e20d5f462af2fe53f832650e06c217170e465e6
kaarthik1629 <kaarthikchillzz@gmail.com>
2024-11-12 20:51:56 +0530

    Initaial commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using JokesWebAppNew.Models;
+
+namespace JokesWebAppNew.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<JokesWebAppNew.Models.Joke> Joke { get; set; } = default!;
+    }
+}
